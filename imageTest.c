2ebee7b2d11aaad4831154d6bbe84c9850db0818
rtavares018 <regina.tavares@ua.pt>
2023-11-26 00:01:00 +0000

    push

--- imageTest.c
+++ imageTest.c
@@ -18,17 +18,20 @@
 #include "image8bit.h"
 #include "instrumentation.h"
 
-int main(int argc, char* argv[]) {
-  if (argc != 3) {
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
     error(1, 0, "Usage: imageTest input.pgm output.pgm");
   }
 
   ImageInit();
-  
+
   printf("# LOAD image");
   InstrReset(); // to reset instrumentation
   Image img1 = ImageLoad(argv[1]);
-  if (img1 == NULL) {
+  if (img1 == NULL)
+  {
     error(2, errno, "Loading %s: %s", argv[1], ImageErrMsg());
   }
   InstrPrint(); // to print instrumentation
@@ -36,17 +39,42 @@ int main(int argc, char* argv[]) {
   // Try changing the behaviour of the program by commenting/uncommenting
   // the appropriate lines.
 
-  //img2 = ImageCrop(img1, ImageWidth(img1)/4, ImageHeight(img1)/4, ImageWidth(img1)/2, ImageHeight(img1)/2);
-  Image img2 = ImageRotate(img1);
-  if (img2 == NULL) {
-    error(2, errno, "Rotating img2: %s", ImageErrMsg());
-  }
+  // img2 = ImageCrop(img1, ImageWidth(img1)/4, ImageHeight(img1)/4, ImageWidth(img1)/2, ImageHeight(img1)/2);
+  // Image img2 = ImageCrop(img1, 5, 78, ImageWidth(img1) / 2, ImageHeight(img1) / 3);
 
-  //ImageNegative(img2);
-  //ImageThreshold(img2, 100);
-  ImageBrighten(img2, 1.3);
+  // test ImageLocateSubImage(Image img1, int *px, int *py, Image img2)
+  // int px, py;
+  // ImageLocateSubImage(img1, &px, &py, img2);
+  // printf("px = %d, py = %d\n", px, py);
 
-  if (ImageSave(img2, argv[2]) == 0) {
+  // if (img2 == NULL)
+  // {
+  //   error(2, errno, "Rotating img2: %s", ImageErrMsg());
+  // }
+
+  Image img2 = ImageLoad(argv[1]);
+
+  // Test ImageBlur(Image img, int dx, int dy)
+  // ImageBlur(img2, 3, 3);
+  // mais blur
+  // InstrReset();
+  // ImageBlur(img2, 5, 5);
+  // InstrPrint();
+  // ainda mais blured
+  // InstrReset();
+  // ImageBlur(img2, 7, 7);
+  // InstrPrint();
+  InstrReset();
+  ImageBlur(img2, 15, 15);
+  InstrPrint();
+
+  // ImageNegative(img2);
+  // ImageThreshold(img2, 100);
+  // ImageBrighten(img2, 1.3);
+  // ImageRotate(img2);
+
+  if (ImageSave(img2, argv[2]) == 0)
+  {
     error(2, errno, "%s: %s", argv[2], ImageErrMsg());
   }
 
@@ -54,4 +82,3 @@ int main(int argc, char* argv[]) {
   ImageDestroy(&img2);
   return 0;
 }
-
